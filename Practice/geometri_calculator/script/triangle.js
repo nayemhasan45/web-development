5db46c93daf682babce5826b1718bab099338690
nayemhasan45 <nayemhasan75907@gmail.com>
2024-03-31 20:13:14 +0100

    js added into html file

---
+++ Practice/geometri_calculator/script/triangle.js
@@ -0,0 +1,8 @@
+document.getElementById('tBtn').addEventListener('click',function(){
+    const baseValue = document.getElementById('tInputB').value;
+    const heightValue = document.getElementById('tInputH').value;
+    const base = parseFloat(baseValue);
+    const height = parseFloat(heightValue);
+    const area = 0.5*base*height;
+    document.getElementById('areaAns').innerText=area;
+})
