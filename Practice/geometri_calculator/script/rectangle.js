340a4272642c9841034e940a5609d1faa3afbaf1
nayemhasan45 <nayemhasan75907@gmail.com>
2024-04-01 15:31:30 +0100

    all problem solved

--- Practice/geometri_calculator/script/rectangle.js
+++ Practice/geometri_calculator/script/rectangle.js
@@ -3,12 +3,12 @@
 //     const lengthValue = parseFloat(document.getElementById('rInputL').value);
 //     document.getElementById('rAreaAns').innerText=widthValue*lengthValue;
 // }
-function rectangleArea(){
+function rectangleArea() {
     const width = getInputValueById('rInputW');
     const length = getInputValueById('rInputL');
-    document.getElementById('rAreaAns').innerText=width*length;
+    document.getElementById('rAreaAns').innerText = width * length;
 }
-function getInputValueById(inputId){
+function getInputValueById(inputId) {
     const value = parseFloat(document.getElementById(inputId).value);
     return value;
 }
