0d4c92cae806b520d4f43fba8ee2a7e5c14c7daa
nayemhasan45 <nayemhasan75907@gmail.com>
2024-04-01 15:17:58 +0100

    final task practice done

--- Practice/geometri_calculator/script/rectangle.js
+++ Practice/geometri_calculator/script/rectangle.js
@@ -1,5 +1,14 @@
+// function rectangleArea(){
+//     const widthValue=parseFloat(document.getElementById('rInputW').value);
+//     const lengthValue = parseFloat(document.getElementById('rInputL').value);
+//     document.getElementById('rAreaAns').innerText=widthValue*lengthValue;
+// }
 function rectangleArea(){
-    const widthValue=parseFloat(document.getElementById('rInputW').value);
-    const lengthValue = parseFloat(document.getElementById('rInputL').value);
-    document.getElementById('rAreaAns').innerText=widthValue*lengthValue;
+    const width = getInputValueById('rInputW');
+    const length = getInputValueById('rInputL');
+    document.getElementById('rAreaAns').innerText=width*length;
+}
+function getInputValueById(inputId){
+    const value = parseFloat(document.getElementById(inputId).value);
+    return value;
 }
