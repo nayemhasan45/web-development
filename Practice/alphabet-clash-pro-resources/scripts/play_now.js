a7d50f61a4d10aff2fcedd8544932a55cde02719
nayemhasan45 <nayemhasan75907@gmail.com>
2024-04-06 21:53:28 +0100

    js event handeler added

--- Practice/alphabet-clash-pro-resources/scripts/play_now.js
+++ Practice/alphabet-clash-pro-resources/scripts/play_now.js
@@ -1,3 +1,14 @@
+function detectChar(event){
+    const playerPressed = event.key;
+    const expectedChar = document.getElementById('randomAlpha').innerText.toLocaleLowerCase();
+    // console.log('player pressed :',playerPressed,'expected char :',expectedChar);
+    if(playerPressed===expectedChar){
+        console.log('you press the write char');
+    }else{
+        console.log('you enter the wrong char')
+    }
+}
+document.addEventListener('keyup',detectChar);
 function gameLoop(){
     const alphabet = getRandomAlpha();
     const p = document.getElementById('randomAlpha');
