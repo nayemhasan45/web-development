4ecc1db438fe2f34c4c22e5cd6a220775c15e2e4
nayemhasan45 <nayemhasan75907@gmail.com>
2024-04-22 22:15:07 +0100

    part 2 done

--- Practice/milestone-6-m-31/task-1.js
+++ Practice/milestone-6-m-31/task-1.js
@@ -18,3 +18,30 @@ let data={
     }
 }
 console.log(data.Shophia.study[1].secondary[1].location);
+// part 2
+let students = {
+    2222:{
+        name:"jack",
+        section: 'c',
+        class: 'ix',
+        adress:{
+            "building no":12,
+            "street":"lalal",
+            "city":"peterburg",
+            "country":'uk'
+        }
+    },
+    3333:{
+        name:'herry',
+        section : 'd',
+        class:'x',
+        adress:{
+            "building no":85,
+            "street":'dc road',
+            'city':'kakka',
+            'country':'bd',
+        }
+    }
+}
+console.log(students[2222].adress.city);
+console.log(students[3333].name)
