0de95fc251b9551a732bba724b5d8139304f9243
nayemhasan45 <nayemhasan75907@gmail.com>
2024-04-22 14:50:11 +0100

    practice problem 1 done in js

---
+++ Practice/milestone_6_m_30/task-1.js
@@ -0,0 +1,13 @@
+// create arrow function 
+// p1
+const multiply = (x,y,z) =>x*y*z;
+console.log(multiply(2,2,2));
+// p2
+const text = `i am a web developer.
+i love to code.
+i love to eat.`;
+console.log(text);
+// p3
+const add=(x,y=0)=>x+y;
+console.log(5);
+ 
